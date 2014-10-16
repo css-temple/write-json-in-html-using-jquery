894e1f703a85b125e450a44d20defa2359a5da80
vladan-m <vladan@mvdevelop.com>
2014-10-16 16:27:15 +0200

    adding project

---
+++ javascripts/common.js
@@ -0,0 +1,31 @@
+/*
+	Copyright 2014, MV Develop
+	http://mvdevelop.com/
+
+	Licensed under the MIT license.
+	http://www.opensource.org/licenses/mit-license.php
+
+	Created by: vladan@mvdevelop.com
+	Maintained by: vladan@mvdevelop.com
+*/
+
+$(document).ready(function(){
+	var startTime = new Date().getTime();
+	$.getJSON( "website.json", function(data) {
+		var $siteList = $(".site-list"),
+				array = [],
+        l = data.length;
+		for(i = 0; i < l; i++) {
+			array[i] = '<li class="clearfix"><div class="image-holder"><img src="' + data[i].img + '" width="300" height="160"></div> <div class="main-info"><h3>' + data[i].title + '</h3><div class="info">' + data[i].info + '</div><p class="url">URL: <a href="' + data[i].url + '">' + data[i].url + '</a></p></div></li>';
+    }
+    $siteList.html(array.join(''));
+
+    $(document).ajaxStop(function() {
+			$('.site-list').find('li:last-child').addClass('last');
+		});
+
+		var endTime = new Date().getTime();
+		console.log(endTime - startTime);
+	});
+
+});
